00ae935523a219a1a3bbbc9c7c46aecbc32712c0
DarekR88 <dradmclad@gmail.com>
2019-03-31 18:31:08 -0500

    changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,31 @@
-$(document).ready(function() {
-  // Get references to the username input and password input
-  var username = $("#username");
-  var password = $("#password");
+// $(document).ready(function() {
+//   // Get references to the username input and password input
+//   var username = $("#username");
+//   var password = $("#password");
 
-  // Add event listeners for sign-up and login buttons
-  $(document).on("click", "#signUp", createUser);
-  $(document).on("click", "#login", loginUser);
-});
+//   // Add event listeners for sign-up and login buttons
+//   $(document).on("click", "#signUp", createUser);
+//   $(document).on("click", "#login", loginUser);
+// });
 
-// This function creates a new user
-function createUser(event) {
-  event.preventDefault();
+// // This function creates a new user
+// function createUser(event) {
+//   event.preventDefault();
 
-  // Don't do anything if the input fields are empty
-  if (!username.val().trim() || !password.val().trim()) {
-    return;
-  } 
+//   // Don't do anything if the input fields are empty
+//   if (!username.val().trim() || !password.val().trim()) {
+//     return;
+//   } 
   
-}
+// }
+
+
+anime ({
+  targets: ['.logo'],
+  rotate: 360,
+  duration: 3000,
+  autoplay: true,
+  elasticity: 650,
+  delay: 500,
+  easing: 'easeOutElastic',
+});
